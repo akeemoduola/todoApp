356f76cd1ffb20adbea71009516c8ec3c7cba68f
Oduola Akeem <(none)>
2016-08-02 11:48:28 +0100

    Update requirements.txt for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ProjectName.wsgi:application --log-file -
+web: gunicorn todoApp.wsgi:application --log-file -
--- todoApp/wsgi.py
+++ todoApp/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
-
+i
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoApp.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
